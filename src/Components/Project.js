ba165e3aaed20a588b9d8933acbdf26455677d5f
micheci <michecimartinez@gmail.com>
2022-11-30 16:36:07 -0600

    starting it

---
+++ src/Components/Project.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Css/Projectcss.css'
+
+const Project = () => {
+  return (
+    <div>
+        <article>
+            <a href='https://www.google.com/'>
+                <p>
+                    Project
+                </p>
+            </a>
+        </article>
+    </div>
+  )
+}
+
+export default Project
---
+++ src/Components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Project from './Project'
+
+const Projects = () => {
+  return (
+    <div className='projects-container'> 
+        <Project />
+        <br></br>
+        <Project/>
+        <br></br>
+        <Project/>
+    </div>
+  )
+}
+
+export default Projects
