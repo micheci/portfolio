536c9596c9a74db8703fb541f045c39f47a3e9d0
micheci <michecimartinez@gmail.com>
2023-03-04 21:58:43 -0600

    added new project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,10 +4,17 @@ import Project from './Project'
 const Projects = () => {
   return (
     <div className='projects-container'> 
+
+        <Project desc="Your go-to for anything financial in your life,from investmenests to spending habits" title="Financial" siteName="https://dos-streams-production.up.railway.app/" lang="JavaScript,EJS" />
+
+        <Project desc="Find the temperature of a locations cordinates!!!" title="Weather EJS" siteName="https://weatherappejs.up.railway.app/" lang="JavaScript,EJS" />
+
         <Project desc="Shows space pictures depending on day in HTML,CSS,JAVASCRIPT"title=" Space pictures" siteName="https://spacesky-production.up.railway.app/" lang="React"/>
         
         <Project desc="Full Stack social media site using ejs/node/express" title="Freedom Social Page" siteName="https://binary-upload-boom-production.up.railway.app/" lang="EJS" />
         
+        <Project desc="Munky NFT Full Stack Site" title="MunkyNFT" siteName="https://cryptosite-production.up.railway.app/" lang="JavaScript,EJS" />
+
         <Project desc="Full Stack page with Auth to watch 2 streams at once" title="Dos Streams" siteName="https://dos-streams-production.up.railway.app/" lang="JavaScript,EJS" />
     </div>
   )
